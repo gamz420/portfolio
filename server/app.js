ac1bc6a82376049cefdabdb1bd1273c437cca425
Gamz <jeydey@mail.ru>
2021-04-23 12:39:27 +0300

    Debug

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { Telegraf } = require("telegraf");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -17,6 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const root = path.join(__dirname, "../", "client", "build");
+
+app.use(express.static(root));
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root });
+});
 
 bot.use(async (ctx, next) => {
   try {
